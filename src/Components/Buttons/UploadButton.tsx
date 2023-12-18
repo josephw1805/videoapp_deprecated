e799c59bb11dcdab9dfb2a33a234362a32f5c7c5
josephW1805 <joseph.weng.1805@gmail.com>
2023-12-17 19:46:48 -0500

    complete

--- src/Components/Buttons/UploadButton.tsx
+++ src/Components/Buttons/UploadButton.tsx
@@ -1,44 +1,40 @@
-import { Dialog, Transition } from "@headlessui/react";
+import { Transition, Dialog } from "@headlessui/react";
+import React, { useState, useRef, Fragment } from "react";
 import { Plus } from "../Icons/Icons";
+import "cropperjs/dist/cropper.css";
 import { Button } from "./Buttons";
-import { Fragment, useRef, useState } from "react";
+import { api } from "~/utils/api";
+import { useSession } from "next-auth/react";
+import { env } from "~/env.mjs";
 
-export default function UploadButton({
-  refetch,
-}: {
-  refetch: () => Promise<unknown>;
-}) {
-  const cancelButtonRef = useRef(null);
+export default function UploadButton({ refetch }: { refetch: () => Promise<unknown> }) {
   const [open, setOpen] = useState(false);
+  const cancelButtonRef = useRef(null);
   const [uploadedVideo, setUploadedVideo] = useState<File | null>(null);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      setUploadedVideo(event.target.files[0] ?? null);
-    }
-  };
+  const { data: sessionData } = useSession();
+  const addVideoUpdateMutation = api.video.createVideo.useMutation();
 
   const handleSubmit = () => {
     type UploadResponse = {
       secure_url: string;
     };
     const videoData = {
-      id: video.id,
-      userId: sessionData?.user.id ?? "",
-      title: video.title ?? undefined,
-      description: video.description ?? undefined,
-      thumbnailUrl: video.thumbnailUrl ?? undefined,
+      userId: sessionData?.user.id as string,
+      videoUrl: "",
     };
 
     const formData = new FormData();
     formData.append("upload_preset", "user_uploads");
-    formData.append("file", croppedImage ?? "");
+
+    if (uploadedVideo) {
+      formData.append("file", uploadedVideo);
+    }
+
     fetch(
-      "https://api.cloudinary.com/v1_1/" + cloudinaryName + "/image/upload",
+      "https://api.cloudinary.com/v1_1//" +
+        env.NEXT_PUBLIC_CLOUDINARY_NAME +
+        "/video/upload",
+
       {
         method: "POST",
         body: formData,
@@ -46,43 +42,49 @@ export default function UploadButton({
     )
       .then((response) => response.json() as Promise<UploadResponse>)
       .then((data) => {
-        if (
-          user.title !== video.title ||
-          user.description !== video.description ||
-          data.secure_url !== undefined
-        ) {
+        console.log("secure_url");
+        console.log(data);
+        if (data.secure_url !== undefined) {
           const newVideoData = {
             ...videoData,
-            ...(data.secure_url && { thumbnailUrl: data.secure_url }),
+            ...(data.secure_url && { videoUrl: data.secure_url }),
           };
-          if (user.title !== video.title) newVideoData.title = user.title;
-          if (user.description !== video.description)
-            newVideoData.description = user.description;
 
           addVideoUpdateMutation.mutate(newVideoData, {
             onSuccess: () => {
-              void refetch();
               setOpen(false);
+              void refetch();
             },
           });
         }
       })
       .catch((error) => {
-        console.log("An error occured: ", error);
+        console.error("An error occurred:", error);
       });
+  };
+
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setUploadedVideo(event.target.files[0] ? event.target.files[0] : null);
+    }
+  };
+
+  const handleClick = () => {
+    setOpen(true);
   };
 
   return (
     <>
       <Button
+        onClick={() => handleClick()}
         variant="primary"
         size="2xl"
         className="ml-2 flex"
-        onClick={() => handleClick()}
       >
         <Plus className="mr-2 h-5 w-5 shrink-0 stroke-white" />
         Upload
       </Button>
+
       <Transition.Root show={open} as={Fragment}>
         <Dialog
           as="div"
@@ -114,61 +116,63 @@ export default function UploadButton({
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mt-3 w-full text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
-                        Upload Video
-                      </Dialog.Title>
-                      <div className="col-span-full">
-                        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                          <div className="text-center">
-                            {uploadedVideo ? (
-                              <p>Your video has been attached.</p>
-                            ) : (
-                              <div>
-                                <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                                  <label
-                                    htmlFor="file-upload"
-                                    className="relative cursor-pointer rounded-md bg-white font-semibold text-primary-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-primary-600 focus-within:ring-offset-2 hover:text-primary-500"
-                                  >
-                                    <span>Upload a Video</span>
-                                    <input
-                                      id="file-upload"
-                                      name="file-upload"
-                                      type="file"
-                                      className="sr-only"
-                                      onChange={onFileChange}
-                                    />
-                                  </label>
-                                  <p className="pl-1">or drag and drop</p>
+                  <>
+                    <div className="sm:flex sm:items-start  ">
+                      <div className="mt-3 w-full text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900"
+                        >
+                          Upload Video{" "}
+                        </Dialog.Title>
+                        <div className="col-span-full">
+                          <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                            <div className="text-center">
+                              {uploadedVideo ? (
+                                <p>Your video has been attached.</p>
+                              ) : (
+                                <div>
+                                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                                    <label
+                                      htmlFor="file-upload"
+                                      className="relative cursor-pointer rounded-md bg-white font-semibold text-primary-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-primary-600 focus-within:ring-offset-2 hover:text-primary-500"
+                                    >
+                                      <span>Upload a Video</span>
+                                      <input
+                                        id="file-upload"
+                                        name="file-upload"
+                                        type="file"
+                                        className="sr-only"
+                                        onChange={onFileChange}
+                                      />
+                                    </label>
+                                    <p className="pl-1">or drag and drop</p>
+                                  </div>
                                 </div>
-                              </div>
-                            )}
+                              )}
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className=" relative mt-5 flex flex-row-reverse gap-2 sm:mt-4 ">
-                    <Button
-                      type="reset"
-                      variant="primary"
-                      size="lg"
-                      onClick={() => handleSubmit()}
-                    >
-                      Upload
-                    </Button>
-                    <Button
-                      variant="secondary-gray"
-                      size="lg"
-                      onClick={() => setOpen(false)}
-                    >
-                      Cancel
-                    </Button>
-                  </div>
+                    <div className=" relative mt-5 flex flex-row-reverse gap-2 sm:mt-4 ">
+                      <Button
+                        type="reset"
+                        variant="primary"
+                        size="lg"
+                        onClick={() => handleSubmit()}
+                      >
+                        Upload
+                      </Button>
+                      <Button
+                        variant="secondary-gray"
+                        size="lg"
+                        onClick={() => setOpen(false)}
+                      >
+                        Cancel
+                      </Button>
+                    </div>
+                  </>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
